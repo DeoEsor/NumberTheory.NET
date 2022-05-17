d144f22c1cf3a30d30df2df55100ad2a5444c674
DeoEsor <RulianValeriy@yandex.ru>
2022-05-17 23:19:12 +0300

    [Number Theory] Interfaces IPrimalChecker.cs

---
+++ NumberTheory/Interfaces/IPrimalChecker.cs
@@ -0,0 +1,15 @@
+﻿using System.Numerics;
+
+namespace NumberTheory.Interfaces;
+
+public interface IPrimalChecker
+{ 
+    bool Check(BigInteger value, float minProbability);
+    
+    public static int GetDegree(float value)
+    {
+        int power = 1;
+        while ((1 - Math.Pow(0.5, power)) < value ) power++;
+        return power;
+    }
+}
