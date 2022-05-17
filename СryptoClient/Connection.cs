9914113a910dee120542a11e40c58ed5ef85fb8a
DeoEsor <RulianValeriy@yandex.ru>
2022-05-17 23:17:15 +0300

    [gRPC Client] init

--- СryptoClient/Connection.cs
+++ СryptoClient/Connection.cs
@@ -3,33 +3,31 @@ using System.Net.Http;
 using CryptoServices;
 using Grpc.Net.Client;
 using Microsoft.Extensions.Logging;
-namespace СryptoClient
+namespace СryptoClient;
+
+public class Connection
 {
-	public class Connection
+	private readonly ILogger<Connection> _logger;
+
+	public string last_string;
+	public Connection(ILogger<Connection> logger = null)
 	{
-		private readonly ILogger<Connection> _logger;
+		_logger = logger;
 
-		public string last_string;
-		public Connection(ILogger<Connection> logger = null)
-		{
-			_logger = logger;
+		var httpHandler = new HttpClientHandler();
 
-			var httpHandler = new HttpClientHandler();
+		httpHandler.ServerCertificateCustomValidationCallback =
 
-			httpHandler.ServerCertificateCustomValidationCallback =
+			// Return `true` to allow certificates that are untrusted/invalid
+			HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+		using var channel = GrpcChannel.ForAddress("https://localhost:5001",
+			 new GrpcChannelOptions { HttpHandler = httpHandler });
 
-				// Return `true` to allow certificates that are untrusted/invalid
-				HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
-			using var channel = GrpcChannel.ForAddress("https://localhost:5001",
-				 new GrpcChannelOptions { HttpHandler = httpHandler });
-
-			var client = new CryptoService.CryptoServiceClient(channel);
-			var reply = client.StartSession(new StartSessionRequest());
-			if (_logger == null)
-				Console.WriteLine(last_string = $"{reply.StatusCode}");
-			else
-				logger.LogInformation(last_string = $"Connection {reply.StatusCode}");
-		}
+		var client = new CryptoService.CryptoServiceClient(channel);
+		var reply = client.StartChatting(new StartSessionRequest());
+		if (_logger == null)
+			Console.WriteLine(last_string = $"{reply.StatusCode}");
+		else
+			logger.LogInformation(last_string = $"Connection {reply.StatusCode}");
 	}
-
 }
