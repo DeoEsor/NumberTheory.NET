28a92ffaf4729cee5afb863fbef8c145a29a821e
DeoEsor <RulianValeriy@yandex.ru>
2022-05-17 23:20:40 +0300

    [Number Theory] FermatTest.cs Solovay–StrassenTest.cs

---
+++ NumberTheory/PrimalCheckers/FermatTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Numerics;
+using NumberTheory.Extensions.Arithmetic;
+using NumberTheory.Interfaces;
+
+namespace NumberTheory.PrimalCheckers;
+
+public class FermatTest : IPrimalChecker
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="value"></param>
+    /// <param name="minProbability"></param>
+    /// <returns>Is <param name="value"/> can be called pseudo-primal </returns>
+    /// <exception cref="NotImplementedException"></exception>
+    public bool Check(BigInteger value, float minProbability)
+    {
+        if (minProbability is not (> 0.49f and < 1))
+            throw new ArgumentException(nameof(minProbability));
+        if (value <= 0)
+            throw new ArgumentException(nameof(value));
+        
+        if (value <= 3)
+            return true;
+        var random = new Random();
+
+        for (int i = 0; i < IPrimalChecker.GetDegree(minProbability); i++) // attempts - кол-во попыток, так как тест - вероятностный 
+        {
+            BigInteger a = random.Next() % (value - 2) + 2;
+		
+            if (BigInteger.GreatestCommonDivisor(a, value) != 1) // если числа не взаимно просты, то понятно, что p не простое 
+                return false;
+		
+            if (ArithmeticExtensions.PowMod(a, value-1,value) != 1) // a^(p-1) = 1 (mod p)
+                return false;
+        }
+
+        return true;
+    }
+}
---
+++ NumberTheory/PrimalCheckers/Solovay–StrassenTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Numerics;
+using NumberTheory.Extensions.Arithmetic;
+using NumberTheory.Interfaces;
+using NumberTheory.Symbols;
+
+namespace NumberTheory.PrimalCheckers;
+
+public class Solovay_StrassenTest : IPrimalChecker
+{
+    public bool Check(BigInteger value, float minProbability)
+    {
+        if (value < 2)
+            return false;
+        if (value != 2 && value % 2 == 0)
+            return false;
+ 
+        var random = new Random();
+        
+        for (int i = 0; i < IPrimalChecker.GetDegree(minProbability); i++)
+        {
+            
+            var a = random.Next() % (value - 1) + 1;
+            var jacobian = (value + Jacobi.J(a, value)) % value;
+            var mod = ArithmeticExtensions.PowMod(a, (value - 1) / 2, value);
+ 
+            if (jacobian == 0 || mod != jacobian)
+                return false;
+        }
+        return true;
+    }
+}
