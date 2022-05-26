894f76121bd5dda66ba5aecbfe914dc03806e36d
DeoEsor <RulianValeriy@yandex.ru>
2022-05-26 15:01:17 +0300

    [Crypto] Des algo

--- CryptographyLib/Symmetric/FeistelNetwork/FeistelNetwork.cs
+++ CryptographyLib/Symmetric/FeistelNetwork/FeistelNetwork.cs
@@ -11,6 +11,17 @@ namespace CryptographyLib.Symmetric.FeistelNetwork
 	/// </summary>
 	public class FeistelNetwork : SymmetricEncryptorBase
 	{
+		private static readonly byte[] E = new byte[]
+		{
+				32,	1,	2,	3,	4,	5,
+				4,	5,	6,	7,	8,	9,
+				8,	9,	10,	11,	12,	13,
+				12,	13,	14,	15,	16,	17,
+				16,	17,	18,	19,	20,	21,
+				20,	21,	22,	23,	24,	25,
+				24,	25,	26,	27,	28,	29,
+				28,	29,	30,	31,	32,	1
+		};
 		private readonly ISymmetricEncryptor _encryptor;
 		public int Rounds
 		{
@@ -20,7 +31,7 @@ namespace CryptographyLib.Symmetric.FeistelNetwork
 		private IPadding Padding = new PKCS7();
 
 		public FeistelNetwork(  IExpandKey expandKey,
-								ISymmetricEncryptor symmetricEncryptor)
+								ISymmetricEncryptor symmetricEncryptor = null!)
 			: base(expandKey)
 		{
 			_encryptor = symmetricEncryptor;
