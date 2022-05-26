44f0c83fe66b5cf86be8ee961ce39d037b83d7ad
DeoEsor <RulianValeriy@yandex.ru>
2022-05-26 14:56:38 +0300

    [NumberTheory] Primal Random Generators

---
+++ NumberTheory/RandomGenerators/BruteForcePrimalRandomGenerator.cs
@@ -0,0 +1,20 @@
+﻿using NumberTheory.Interfaces;
+using NUnit.Framework;
+namespace NumberTheory.RandomGenerators;
+
+public class BruteForcePrimalRandomGenerator : PrimalRandomGenerator
+{
+    public BruteForcePrimalRandomGenerator(IPrimalChecker primalChecker) 
+        : base(primalChecker)
+    {}
+
+    public override BigInteger Generate()
+    {
+        var value = BigInteger.Pow(TestContext.CurrentContext.Random.NextULong(), 2);;
+        
+        while (!PrimalChecker.Check(value, 0.95f))
+            value = BigInteger.Pow(TestContext.CurrentContext.Random.NextULong(), 2);
+
+        return value;
+    }
+}
---
+++ NumberTheory/RandomGenerators/PrimalRandomGenerator.cs
@@ -0,0 +1,15 @@
+﻿using NumberTheory.Interfaces;
+
+namespace NumberTheory.RandomGenerators;
+
+public abstract class PrimalRandomGenerator : IRandomGenerator
+{
+    public IPrimalChecker PrimalChecker { get; set; }
+    
+    protected PrimalRandomGenerator(IPrimalChecker primalChecker)
+    {
+        PrimalChecker = primalChecker;
+    }
+
+    public abstract BigInteger Generate();
+}
