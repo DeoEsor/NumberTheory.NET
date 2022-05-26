f9d640054c2e6e7a1f05043b3c4d16800c44ad6f
DeoEsor <RulianValeriy@yandex.ru>
2022-05-26 14:59:18 +0300

    [Crypto] Key generator interfaces

---
+++ CryptographyLib/Interfaces/IKeyGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Numerics;
+
+namespace CryptographyLib.Interfaces;
+
+public interface IKeyGenerator<T>
+{
+    byte[] Generate(T[] values);
+    
+    Task<byte[]> GenerateAsync(T[] values);
+}
+
+public interface IKeyGenerator
+{
+    byte[] Generate(params object[] value);
+    
+    Task<byte[]> GenerateAsync(params object[] value);
+}
