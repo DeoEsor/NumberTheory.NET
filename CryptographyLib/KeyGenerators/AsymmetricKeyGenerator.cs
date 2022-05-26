cb65754b0ea9904e8d1e691ea25c7363dd9378cd
DeoEsor <RulianValeriy@yandex.ru>
2022-05-26 15:01:34 +0300

    [Crypto] Key generators

---
+++ CryptographyLib/KeyGenerators/AsymmetricKeyGenerator.cs
@@ -0,0 +1,19 @@
+﻿using CryptographyLib.Interfaces;
+
+namespace CryptographyLib.KeyGenerators;
+
+public abstract class AsymmetricKeyGenerator 
+{
+    public IKeyGenerator PublicKeyGenerator { get; set; }
+    public IKeyGenerator PrivateKeyGenerator { get; set; }
+    
+    public AsymmetricKeyGenerator(IKeyGenerator privateKeyGenerator, IKeyGenerator publicKeyGenerator)
+    {
+        PrivateKeyGenerator = privateKeyGenerator;
+        PublicKeyGenerator = publicKeyGenerator;
+    }
+
+    public virtual byte[] CreatePublicKey(params object[] value) => PublicKeyGenerator.Generate();
+
+    public virtual byte[] CreatePrivateKey(params object[] value)=> PrivateKeyGenerator.Generate();
+}
---
+++ CryptographyLib/KeyGenerators/RSAKeyGenerator.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Numerics;
+using CryptographyLib.Extensions;
+using CryptographyLib.Interfaces;
+using NumberTheory.Euclid;
+using NumberTheory.PrimalCheckers;
+using NumberTheory.RandomGenerators;
+using NumberTheory.Symbols;
+
+namespace CryptographyLib.KeyGenerators;
+
+public sealed class RSAKeyGenerator : AsymmetricKeyGenerator
+{
+    private BigInteger _e;
+
+
+    private Lazy<((BigInteger, BigInteger), (BigInteger, BigInteger))> GeneratedPair;
+    public BigInteger E
+    {
+        get => _e;
+        set
+        {
+            _e = value;
+            GeneratedPair = new Lazy<((BigInteger, BigInteger), (BigInteger, BigInteger))>(Init);
+        }
+    }
+
+    private PrimalRandomGenerator Generator { get; set; } 
+    public RSAKeyGenerator(BigInteger e,PrimalRandomGenerator generator = null!, 
+        IKeyGenerator privateKeyGenerator = default!, 
+        IKeyGenerator publicKeyGenerator = default!)
+        : base(privateKeyGenerator, publicKeyGenerator)
+    {
+        E = e;
+        Generator = generator ?? new BruteForcePrimalRandomGenerator(new MillerRabinTest());
+    }
+
+    public override byte[] CreatePrivateKey(params object[] value)
+        =>
+            new BitArray(GeneratedPair.Value.Item2.Item1.ToByteArray())
+                .Concat(new BitArray(GeneratedPair.Value.Item2.Item2.ToByteArray()))
+                .ToBytes();
+
+    public override byte[] CreatePublicKey(params object[] value)
+        =>
+            new BitArray(GeneratedPair.Value.Item1.Item1.ToByteArray())
+                .Concat(new BitArray(GeneratedPair.Value.Item1.Item2.ToByteArray()))
+                .ToBytes();
+
+    ((BigInteger, BigInteger), (BigInteger, BigInteger)) Init()
+    {
+        Random random = new Random();
+        var p = Generator.Generate();
+        var q = Generator.Generate();
+        var mod = p * q;
+        var phi = (p-1) * (q  - 1);
+        if (ExtendedGCD.Solve(E, phi, out var _, out var _) == 1)
+            while (true)
+            {
+                _e = random.Next(1, (int)(phi - 1));
+                var gcd = ExtendedGCD.Solve(E, phi, out var _, out var _);
+                if (gcd == 1)
+                    break;
+            }
+        return ((_e,mod),(Euler.EulerFunc(_e),mod));
+    }
+    
+}
