e6ac9dedd2e9923664ebe945121a24274b6d43f6
DeoEsor <RulianValeriy@yandex.ru>
2022-05-26 15:02:50 +0300

    [Crypto] Wiener Attack

---
+++ CryptographyLib/Attacks/WienerAttack.cs
@@ -0,0 +1,34 @@
+﻿using System.Numerics;
+using NumberTheory.Extensions;
+
+namespace CryptographyLib.Attacks;
+
+public class WienerAttack
+{
+
+        public List<(BigInteger, BigInteger)> Attack(BigInteger isN, BigInteger isE) 
+        {
+            var limitD = isN.Sqrt();
+            limitD = limitD.Sqrt();
+            limitD = limitD/3;
+            var _M = 0x01010101;
+            
+            return BigIntegerExtensions
+                .CFRAC(isE, isN)
+                .GetConvergent()
+                .Where(s =>
+                {
+                    if (s.Item2 > limitD)
+                        return false;
+                    var c = BigInteger
+                        .ModPow(_M, isE, isN);
+                    
+                    var m = BigInteger
+                        .ModPow(c, s.Item2, isN);
+
+                    return _M == m;
+
+                })
+                .ToList();
+        }
+}
