03f9207589b0e2f8a19cd69c60c2eedbc7f6b4b3
Valeriy Vartumyan <vdvartumyan@mai.education>
2022-09-03 15:36:10 +0300

    realized ElGamal

--- CryptographyLib/Asymmetric/ElGamal.cs
+++ CryptographyLib/Asymmetric/ElGamal.cs
@@ -1,6 +1,10 @@
-﻿using CryptographyLib.Interfaces;
+﻿using System.Numerics;
+using CryptographyLib.Data;
+using CryptographyLib.Interfaces;
 using CryptographyLib.KeyExpanders;
 using CryptographyLib.KeyGenerators;
+using NumberTheory.Extensions;
+using NumberTheory.PrimalCheckers;
 using NumberTheory.RandomGenerators;
 
 
@@ -8,19 +12,43 @@ namespace CryptographyLib.Asymmetric;
 
 public class ElGamal : IAsymmetricEncryptor
 {
-	public AsymmetricKeyGenerator Generator { get; set; }
-	
+	public AsymmetricKeyGenerator Generator { get; set; } = new ElGamalKeyGenerator();
+
 	public IExpandKey ExpandKey { get; set; }
 	
-	private PrimalRandomGenerator Random { get; set; }
+	private PrimalRandomGenerator Random { get; set; } = new BruteForcePrimalRandomGenerator( new MillerRabinTest());
+	
+	private Key Key { get; set; }
 	
 	public byte[] Encrypt(byte[] value)
 	{
-		
+		var nums = Key.GetBigInts(Key.PublicKey);
+		BigInteger y = nums[0], g = nums[1], p = nums[2]; 
+		var M = new BigInteger(value);
+
+		var k = Random.Generate(1, p - 1);
+
+		while (BigInteger.GreatestCommonDivisor(k, p-1) != 1)
+			k = Random.Generate(1, p - 1);
+
+		var a = BigInteger.ModPow(g , k , p);
+		var b = (BigIntegerExtensions.Pow(y, k ) * M) % p ;
+
+		return Key.SerializeBigInts(new[] { a, b });
 	}
 
 	public byte[] Decrypt(byte[] value)
 	{
-		throw new NotImplementedException();
+		var nums = Key.GetBigInts(value);
+		var p = Key.GetBigInts(Key.PublicKey)[2];
+		var x = Key.GetBigInts(Key.PrivateKey)[0];
+		BigInteger a = nums[0], b = nums[1];
+
+		var m = BigInteger
+			        .Multiply(a, BigIntegerExtensions.Pow(a, p - 1 - x)) 
+		        % p;
+
+		return m
+			.ToByteArray();
 	}
 }
